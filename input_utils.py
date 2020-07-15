72beb25af8b6c52dc9579bc9bc1b2584c1c86c78
cdj0311 <chendajun0311@126.com>
2020-07-15 16:56:28 +0800

    Update input_utils.py

--- input_utils.py
+++ input_utils.py
@@ -44,6 +44,6 @@ def eval_input_fn(filenames=None,
     files = tf.gfile.Glob(filenames)
     dataset = tf.data.TFRecordDataset(files)
     dataset = dataset.map(parse_exp, num_parallel_calls=4)
-    dataset = dataset.batch(batch_size, drop_remainder=True)
+    dataset = dataset.batch(batch_size, drop_remainder=True)。repeat()
     return dataset
 
