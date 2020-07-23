271c5831d2e5af03d4b158d06de651fa8d2b5eef
cdj0311 <chendajun0311@126.com>
2020-07-23 15:29:34 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 #############################################
 # FileName: predict.py
-# Author: cdj0311
+# Author: ChenDajun
 # CreateTime: 2020-06-12
 # Descreption: get sentence vector
 #############################################
