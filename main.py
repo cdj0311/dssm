30a507ebd334610fb7818e60e45550813b3594cd
cdj0311 <chendajun0311@126.com>
2020-07-23 15:28:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # encoding:utf-8
 #############################################
 # FileName: main.py
-# Author: cdj0311
+# Author: ChenDajun
 # CreateTime: 2020-06-12
 # Descreption: train and predict model
 #############################################
